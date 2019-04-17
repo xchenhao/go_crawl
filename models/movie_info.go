02e77db82e3bba8d58b5b14d9a93d61173851913
xchenhao <chenhao_apple@163.com>
2019-04-17 23:07:56 +0800

    init

---
+++ models/movie_info.go
@@ -0,0 +1,156 @@
+package models
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/astaxie/beego/orm"
+	"regexp"
+	"strings"
+	"fmt"
+)
+
+const DB_HOST = "127.0.0.1"
+const DB_PORT = 3306
+const DB_NAME = "test"
+const DB_USER = "root"
+const DB_PWD = "123"
+
+var (
+	db orm.Ormer
+)
+
+type MovieInfo struct{ 
+  Id int64
+  Movie_id int64
+  Movie_name string
+  Movie_pic string
+  Movie_director string
+  Movie_writer string
+  Movie_country string
+  Movie_language string
+  Movie_main_character string
+  Movie_type string
+  Movie_on_time string
+  Movie_span string
+  Movie_grade string
+}
+
+func init() {
+	orm.Debug = true // 是否开启调试模式 调试模式下会打印出sql语句
+	var dbDsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8", DB_USER, DB_PWD, DB_HOST, DB_PORT, DB_NAME)
+	orm.RegisterDataBase("default", "mysql", dbDsn, 30)
+	orm.RegisterModel(new(MovieInfo))
+	db = orm.NewOrm()
+}
+
+func AddMovie(movie_info *MovieInfo)(int64,error){
+	movie_info.Id = 0
+	id,err := db.Insert(movie_info)
+	return id,err
+}
+
+func GetMovieDirector(movieHtml string) string{
+	if movieHtml == ""{
+		return ""
+	}
+
+
+	reg := regexp.MustCompile(`<a.*?rel="v:directedBy">(.*?)</a>`)
+	result := reg.FindAllStringSubmatch(movieHtml, -1)
+
+	if len(result) == 0{
+		return ""
+	}
+
+	return string(result[0][1])
+}
+
+func GetMovieName(movieHtml string)string{
+	if movieHtml == ""{
+		return ""
+	}
+
+	reg := regexp.MustCompile(`<span\s*property="v:itemreviewed">(.*?)</span>`)
+	result := reg.FindAllStringSubmatch(movieHtml, -1)
+
+	if len(result) == 0{
+		return ""
+	}
+
+	return string(result[0][1])
+}
+
+func GetMovieMainCharacters(movieHtml string)string{
+	reg := regexp.MustCompile(`<a.*?rel="v:starring">(.*?)</a>`)
+	result := reg.FindAllStringSubmatch(movieHtml, -1)
+
+	if len(result) == 0{
+		return ""
+	}
+
+	mainCharacters := ""
+	for _,v := range result{
+		mainCharacters += v[1] + "/"
+	}
+
+	return strings.Trim(mainCharacters, "/")
+}
+
+func GetMovieGrade(movieHtml string)string{
+	reg := regexp.MustCompile(`<strong.*?property="v:average">(.*?)</strong>`)
+	result := reg.FindAllStringSubmatch(movieHtml, -1)
+
+	if len(result) == 0{
+		return ""
+	}
+	return string(result[0][1])
+}
+
+func GetMovieGenre(movieHtml string)string{
+	reg := regexp.MustCompile(`<span.*?property="v:genre">(.*?)</span>`)
+	result := reg.FindAllStringSubmatch(movieHtml, -1)
+
+	if len(result) == 0{
+		return ""
+	}
+
+	movieGenre := ""
+	for _,v := range result{
+		movieGenre += v[1] + "/"
+	}
+	return strings.Trim(movieGenre, "/")
+}
+
+func GetMovieOnTime(movieHtml string) string{
+	reg := regexp.MustCompile(`<span.*?property="v:initialReleaseDate".*?>(.*?)</span>`)
+	result := reg.FindAllStringSubmatch(movieHtml, -1)
+
+	if len(result) == 0{
+		return ""
+	}
+
+	return string(result[0][1])
+}
+
+func GetMovieRunningTime(movieHtml string) string{
+	reg := regexp.MustCompile(`<span.*?property="v:runtime".*?>(.*?)</span>`)
+	result := reg.FindAllStringSubmatch(movieHtml, -1)
+
+	if len(result) == 0{
+		return ""
+	}
+
+	return string(result[0][1])
+}
+
+
+func GetMovieUrls(movieHtml string)[]string{
+	reg := regexp.MustCompile(`<a.*?href="(https://movie.douban.com/.*?)"`)
+	result := reg.FindAllStringSubmatch(movieHtml, -1)
+
+	var movieSets []string
+	for _,v := range result{
+		movieSets = append(movieSets, v[1])
+	}
+
+	return movieSets
+}
